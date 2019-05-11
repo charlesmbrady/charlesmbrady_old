03d5352c10d6c2c6f8a553f6d1f3998f2f9d1301
Charles <charlesmbrady@gmail.com>
2019-05-10 22:38:12 -0400

    testing adding react componnent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      put app stuff here!
+      <Home />
     </div>
   );
 }
