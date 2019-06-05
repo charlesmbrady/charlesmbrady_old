7b4dc4b90830356cc170bebb1deac9d1554ea890
Charles <charlesmbrady@gmail.com>
2019-06-05 04:27:48 -0400

    more changes better format

--- build/precache-manifest.b1482c0b1dca315e5c9287b37a3e4b5e.js
+++ build/precache-manifest.b1482c0b1dca315e5c9287b37a3e4b5e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd35229a761c407bdf19c4ab39ad24ea",
+    "revision": "5ac5951b2253594472275b0c09e7e873",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "d836e90288bc68efaa18",
-    "url": "/static/css/main.258cfe53.chunk.css"
+    "revision": "8cbeffc76100f57fb0a3",
+    "url": "/static/css/main.854d40fb.chunk.css"
   },
   {
     "revision": "cab8b1b9eb8a570f89da",
     "url": "/static/js/2.2dad3ab3.chunk.js"
   },
   {
-    "revision": "d836e90288bc68efaa18",
-    "url": "/static/js/main.8a66b986.chunk.js"
+    "revision": "8cbeffc76100f57fb0a3",
+    "url": "/static/js/main.0ab87105.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
