e0dcd256e3c194342fe52416e0240aae6968a9a3
Charles <charlesmbrady@gmail.com>
2019-06-11 13:03:30 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92caa5e9837dbbd8f598c60f61a5e6f0.js"
+  "/precache-manifest.595a762a340e6552cd896d404c37ef52.js"
 );
 
 self.addEventListener('message', (event) => {
