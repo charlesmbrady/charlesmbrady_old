e616c805ee74a5e09da9979349cbd1a326e5b81b
Charles <charlesmbrady@gmail.com>
2020-02-07 17:34:20 -0500

    add build back

---
+++ build/precache-manifest.eaac9d0c4307d3553f50f1f3092fabbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7459b667b3cc9c83a70c6cc92f2655ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb66af2ab228a19a391",
+    "url": "/static/css/main.e751965e.chunk.css"
+  },
+  {
+    "revision": "2f0b259c3d90cc907bcd",
+    "url": "/static/js/2.afdaf78c.chunk.js"
+  },
+  {
+    "revision": "0cb66af2ab228a19a391",
+    "url": "/static/js/main.6eda122e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
